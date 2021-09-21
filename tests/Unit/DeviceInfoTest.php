dc6af9b375eba897b9501eee18bf59407e17bdca
GivMartin <84376344+MartinKKH@users.noreply.github.com>
2021-09-21 11:00:18 +0100

    Remove phpunit env values

--- tests/Unit/DeviceInfoTest.php
+++ tests/Unit/DeviceInfoTest.php
@@ -9,10 +9,4 @@ class DeviceInfoTest extends TestCase {
         $service = new DeviceInfo($deviceId);
         $this->assertEquals($service->getUrl(), 'https://openapi.tuyaeu.com/v1.0/devices/03001768c4dd573e579a');
     }
-    public function test2(): void {
-        $deviceId = getenv('DEVICE_ID');
-        $service = new DeviceInfo($deviceId);
-        $signUrl = $service->getSignUrl();
-        $this->assertEquals(5, 5);
-    }
 }
